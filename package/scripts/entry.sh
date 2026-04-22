8e62705f83fee4437064ad2dfaaca8063a6caa68
Dave G <dpg3k@virginia.edu>
2026-04-22 15:30:16 -0400

    Package and deploy assets

---
+++ package/scripts/entry.sh
@@ -0,0 +1,19 @@
+#
+#
+#
+
+# run application
+cd bin; ./aptrust-submit-client \
+   -port      ${SERVICE_PORT} \
+   -jwtkey    ${JWT_KEY} \
+   -busname   ${EVENT_BUS_NAME} \
+   -eventsrc  ${EVENT_SRC_NAME} \
+   -dbhost    ${DB_HOST} \
+   -dbport    ${DB_PORT} \
+   -dbname    ${DB_NAME} \
+   -dbuser    ${DB_USER} \
+   -dbpass    ${DB_PASSWORD}
+
+#
+# end of file
+#
